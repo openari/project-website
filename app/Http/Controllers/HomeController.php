81a3e54b76a9dad7dab27536781d869d7b177fb6
Jack Tsai <jacksctsai@gmail.com>
2019-04-16 11:43:19 +0800

    add HomeController.php

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Redirect;
+use Illuminate\Http\Request;
+use App\Services\APIService;
+
+class HomeController extends Controller
+{
+    public function __construct() {
+        $this->apiService = new APIService();
+    }
+
+    public function index() {
+        $response = $this->apiService->list_arts();
+        $arts = $response->arts;
+        return view('index', [
+            'arts' => $arts,
+        ]);
+    }
+
+}
